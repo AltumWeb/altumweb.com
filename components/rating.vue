5820bd9d1fed011fa9d0d38f8f298a02c5ef9b95
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-10-07 15:37:02 +0200

    feat: responsive section for rating

--- components/rating.vue
+++ components/rating.vue
@@ -1,3 +1,61 @@
 <template>
+    <div class="py-16">
+        <div class="w-full px-4 md:px-[10%] flex mt-10">
+            <h2 class="text-2xl md:text-4xl font-bold text-light-text dark:text-dark-text mb-20">
+                Ils nous ont fait confiance
+            </h2>
+        </div>
 
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+                    <div class="text-gray-700 font-medium">
+                        Mathieu L.
+                    </div>
+                    <div class="text-sm text-gray-500 mb-4">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                    </div>
+                    <div class="bg-white p-4 rounded-md shadow-inner">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolore ducimus maiores neque nisi quas sapiente sed, temporibus? Molestias, quidem sunt. Beatae culpa enim hic, minus odio placeat reiciendis saepe.</p>
+                    </div>
+                </div>
+
+                <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+                    <div class="text-gray-700 font-medium">
+                        Alison N.
+                    </div>
+                    <div class="text-sm text-gray-500 mb-4">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                    </div>
+                    <div class="bg-white p-4 rounded-md shadow-inner">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet animi doloribus eaque eos est eveniet fugiat illum, ipsam iusto maxime odio odit optio possimus praesentium recusandae reprehenderit sint ut.</p>
+                    </div>
+                </div>
+
+                <div class="bg-gray-200 p-4 rounded-lg shadow-md">
+                    <div class="text-gray-700 font-medium">
+                        Léo T.
+                    </div>
+                    <div class="text-sm text-gray-500 mb-4">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                    </div>
+                    <div class="bg-white p-4 rounded-md shadow-inner">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A earum eveniet, expedita facilis ipsa laborum libero molestias nam, nisi perspiciatis sequi vitae voluptate? Commodi eveniet incidunt maiores officia omnis saepe.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
