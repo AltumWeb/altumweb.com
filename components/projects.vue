4364bbb4d226ea70f03e1ea087e89ee7ed180b0c
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-10-07 15:15:12 +0200

    fix: delete comments

--- components/projects.vue
+++ components/projects.vue
@@ -7,7 +7,6 @@
         </div>
 
         <div class="w-full px-4 md:px-[10%] flex flex-col mt-10 space-y-10">
-            <!-- Première carte projet -->
             <div class="grid grid-cols-1 md:grid-cols-5 rounded-md border-2 border-light-primary hover:scale-[101%] hover:bg-gray-50 dark:hover:bg-gray-950 duration-100 ease-in">
                 <div class="md:col-span-2">
                     <img src="/assets/codeclipper.webp" alt="CodeClipper" class="w-full h-full object-cover md:rounded-l-[3px] bg-light-primary dark:bg-dark-primary">
@@ -27,7 +26,6 @@
                 </div>
             </div>
 
-            <!-- Deuxième carte projet (alternée sur desktop, image en haut sur mobile) -->
             <div class="grid grid-cols-1 md:grid-cols-5 rounded-md border-2 border-light-primary hover:scale-[101%] hover:bg-gray-50 dark:hover:bg-gray-950 duration-100 ease-in">
                 <div class="md:col-span-3 flex flex-col justify-center order-last md:order-none">
                     <div class="flex justify-between px-5 py-3 w-full">
@@ -47,7 +45,6 @@
                 </div>
             </div>
 
-            <!-- Troisième carte projet -->
             <div class="grid grid-cols-1 md:grid-cols-5 rounded-md border-2 border-light-primary hover:scale-[101%] hover:bg-gray-50 dark:hover:bg-gray-950 duration-100 ease-in">
                 <div class="md:col-span-2">
                     <img src="/assets/codeclipper.webp" alt="CodeClipper" class="w-full h-full object-cover md:rounded-l-[3px]">
@@ -67,7 +64,6 @@
                 </div>
             </div>
 
-            <!-- Quatrième carte projet (alternée sur desktop, image en haut sur mobile) -->
             <div class="grid grid-cols-1 md:grid-cols-5 rounded-md border-2 border-light-primary hover:scale-[101%] hover:bg-gray-50 dark:hover:bg-gray-950 duration-100 ease-in">
                 <div class="md:col-span-3 flex flex-col justify-center order-last md:order-none">
                     <div class="flex justify-between px-5 py-3 w-full">
