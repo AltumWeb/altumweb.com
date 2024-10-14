b96f4a6fda56c48236e72f3053a89aa06c7aa226
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-10-14 10:42:17 +0200

    feat: add og for social media

--- pages/mentions-legales.vue
+++ pages/mentions-legales.vue
@@ -8,7 +8,13 @@ useHead({
     ],
     meta: [
         { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+        { name: 'description', content: "AltumWeb est une agence de développement Web basée à Troyes." },
+        { property: 'og:image', content: 'https://altumweb.com/assets/Logo%20clair%20sans%20fond.png' },
+        { name: 'keywords', content: 'altumweb, altum, agence, web, troyes, site, refonte, seo' },
+        { property: 'og:title', content: 'AltumWeb | Agence Web à Troyes' },
+        { property: 'og:description', content: "AltumWeb est une agence de développement Web basée à Troyes." },
+        { name: 'author', content: 'AltumWeb' }
     ],
     script: [
         { src: 'https://kit.fontawesome.com/f89af89e77.js', crossorigin: 'anonymous' }
