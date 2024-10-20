3129bbd18d9f6a05bd567e082a1e8199e7116730
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-10-20 10:54:37 +0200

    feat: install dotenv on project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/dotenv'],
   shadcn: {
     /**
      * Prefix for all the imported component
