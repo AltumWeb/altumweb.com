41e50f7df62d7499b25cd5e853df02b5e151949f
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-10-22 14:22:48 +0200

    fix: dark mode fix for roadmap.vue

--- components/roadmap.vue
+++ components/roadmap.vue
@@ -9,66 +9,66 @@
 
         <ol class="relative border-s border-gray-200 dark:border-gray-700">
             <li class="mb-10 ms-6">
-                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-primary text-light-background dark:bg-dark-primary dark:text-dark-background rounded-full -start-3 ring-8 ring-white dark:ring-gray-900">
+                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-background text-light-text dark:bg-dark-background dark:text-dark-text rounded-full -start-3">
                     <i class="fas fa-comments rounded-full shadow-lg"></i>
                 </span>
                 <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
                     <p class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">Étape 1</p>
-                    <div class="text-sm font-normal text-gray-500 dark:text-gray-300">Décrivez le site web de vos rêves par écrit ou par appel gratuit de 30 minutes.</div>
+                    <div class="text-sm font-normal text-light-text dark:text-dark-text">Décrivez le site web de vos rêves par écrit ou par appel gratuit de 30 minutes.</div>
                 </div>
             </li>
             <li class="mb-10 ms-6">
-                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-primary text-light-background dark:bg-dark-primary dark:text-dark-background rounded-full -start-3 ring-8 ring-white dark:ring-gray-900">
+                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-background text-light-text dark:bg-dark-background dark:text-dark-text rounded-full -start-3">
                     <i class="fas fa-file-invoice-dollar rounded-full shadow-lg"></i>
                 </span>
                 <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
                     <p class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">Étape 2</p>
-                    <div class="text-sm font-normal text-gray-500 dark:text-gray-300">Obtenez un devis gratuit pour la création de votre projet.</div>
+                    <div class="text-sm font-normal text-light-text dark:text-dark-text">Obtenez un devis gratuit pour la création de votre projet.</div>
                 </div>
             </li>
             <li class="mb-10 ms-6">
-                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-primary text-light-background dark:bg-dark-primary dark:text-dark-background rounded-full -start-3 ring-8 ring-white dark:ring-gray-900">
+                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-background text-light-text dark:bg-dark-background dark:text-dark-text rounded-full -start-3">
                     <i class="fas fa-file-signature rounded-full shadow-lg"></i>
                 </span>
                 <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
                     <p class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">Étape 3</p>
-                    <div class="text-sm font-normal text-gray-500 dark:text-gray-300">Signature du devis</div>
+                    <div class="text-sm font-normal text-light-text dark:text-dark-text">Signature du devis</div>
                 </div>
             </li>
             <li class="mb-10 ms-6">
-                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-primary text-light-background dark:bg-dark-primary dark:text-dark-background rounded-full -start-3 ring-8 ring-white dark:ring-gray-900">
+                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-background text-light-text dark:bg-dark-background dark:text-dark-text rounded-full -start-3">
                     <i class="fas fa-pencil-ruler rounded-full shadow-lg"></i>
                 </span>
                 <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
                     <p class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">Étape 4</p>
-                    <div class="text-sm font-normal text-gray-500 dark:text-gray-300">Création de la maquette avec retours illimités (facultatif).</div>
+                    <div class="text-sm font-normal text-light-text dark:text-dark-text">Création de la maquette avec retours illimités (facultatif).</div>
                 </div>
             </li>
             <li class="mb-10 ms-6">
-                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-primary text-light-background dark:bg-dark-primary dark:text-dark-background rounded-full -start-3 ring-8 ring-white dark:ring-gray-900">
+                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-background text-light-text dark:bg-dark-background dark:text-dark-text rounded-full -start-3">
                     <i class="fas fa-code rounded-full shadow-lg"></i>
                 </span>
                 <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
                     <p class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">Étape 5</p>
-                    <div class="text-sm font-normal text-gray-500 dark:text-gray-300">Développement de votre projet avec retours illimités.</div>
+                    <div class="text-sm font-normal text-light-text dark:text-dark-text">Développement de votre projet avec retours illimités.</div>
                 </div>
             </li>
             <li class="mb-10 ms-6">
-                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-primary text-light-background dark:bg-dark-primary dark:text-dark-background rounded-full -start-3 ring-8 ring-white dark:ring-gray-900">
+                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-background text-light-text dark:bg-dark-background dark:text-dark-text rounded-full -start-3">
                     <i class="fas fa-globe rounded-full shadow-lg"></i>
                 </span>
                 <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
                     <p class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">Étape 6</p>
-                    <div class="text-sm font-normal text-gray-500 dark:text-gray-300">Livraison du projet (brut ou avec hébergement inclus).</div>
+                    <div class="text-sm font-normal text-light-text dark:text-dark-text">Livraison du projet (brut ou avec hébergement inclus).</div>
                 </div>
             </li>
             <li class="mb-10 ms-6">
-                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-primary text-light-background dark:bg-dark-primary dark:text-dark-background rounded-full -start-3 ring-8 ring-white dark:ring-gray-900">
+                <span class="absolute flex items-center justify-center w-6 h-6 bg-light-background text-light-text dark:bg-dark-background dark:text-dark-text rounded-full -start-3">
                     <i class="fas fa-tools rounded-full shadow-lg"></i>
                 </span>
                 <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
                     <p class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">Étape 7</p>
-                    <div class="text-sm font-normal text-gray-500 dark:text-gray-300">Suivi et maintenance de votre projet au fil du temps.</div>
+                    <div class="text-sm font-normal text-light-text dark:text-dark-text">Suivi et maintenance de votre projet au fil du temps.</div>
                 </div>
             </li>
         </ol>
